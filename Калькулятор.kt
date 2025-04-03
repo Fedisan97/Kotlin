07e06b9c8f1e9defd1a633d68921c7ab1983fb25
Alexander <20alexxander05@gmail.com>
2025-04-04 02:27:29 +0300

    Create  Калькулятор.kt

---
+++ Калькулятор.kt
@@ -0,0 +1,16 @@
+fun main() {
+    print("Введите первое число: ")
+    val num1 = readLine()?.toDoubleOrNull() ?: 0.0
+    print("Введите второе число: ")
+    val num2 = readLine()?.toDoubleOrNull() ?: 0.0
+    print("Выберите операцию (+, -, *, /): ")
+    val op = readLine()
+    val result = when (op) {
+        "+" -> num1 + num2
+        "-" -> num1 - num2
+        "*" -> num1 * num2
+        "/" -> if (num2 != 0.0) num1 / num2 else "Ошибка деления на ноль"
+        else -> "Неверная операция"
+    }
+    println("Результат: $result")
+}
